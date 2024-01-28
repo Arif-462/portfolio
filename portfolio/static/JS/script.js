427944b7fa155e7497d4b46397bcaca2f73ca7da
Newcoderhut <ariful10462@gmail.com>
2024-01-28 23:10:29 +0300

    portfolio

---
+++ portfolio/static/JS/script.js
@@ -0,0 +1,13 @@
+
+// text amimation
+
+const typed = new Typed('.multi-profession', {
+    strings : ['Software Developer', 'Software Engineer', 'Python Developer', 'Django Developer',
+    'Web Developer', 'Fullstack Developer', 'Backend Developer'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:2000,
+    loop: true
+});
+
+console.log(typed);
