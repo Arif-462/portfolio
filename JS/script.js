9b484fd1133113d530e64d532b4b98a0fbbfe358
Newcoderhut <ariful10462@gmail.com>
2024-03-08 23:25:40 +0300

    updated code

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,47 @@
 
-// text amimation
+//@ts-nocheck
 
+// Togle menuIcon
+
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+console.log(menuIcon);
+
+menuIcon.onclick = () =>{
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+// navbar scrolling
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll("header nav a")
+
+window.onscroll = () => {
+    sections.forEach(sec =>{
+        let top = window.scrollY;
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // Remove toggle and navbar link when click the link
+
+    menuIcon.classList.remove('fa-xmark');
+    navbar.classList.remove('active');
+};
+
+
+
+
+// profession text amimation
 const typed = new Typed('.multi-profession', {
     strings : ['Software Developer', 'Software Engineer', 'Python Developer', 'Django Developer',
     'Web Developer', 'Fullstack Developer', 'Backend Developer'],
@@ -10,4 +51,4 @@ const typed = new Typed('.multi-profession', {
     loop: true
 });
 
-console.log(typed);
+// console.log(typed);
